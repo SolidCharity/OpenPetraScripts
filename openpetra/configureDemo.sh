b4c6cec2947692cfd002563f46b45967a8279f5b
Timotheus Pokorra <tp@tbits.net>
2017-02-07 06:44:38 +0100

    add scripts for configuring demo.openpetra.org

---
+++ openpetra/configureDemo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Author: Timotheus Pokorra <tp@tbits.net>
+# Copyright: 2017 TBits.net
+# Description: create the server for demo.openpetra.org
+
+# setup the instance
+export URL=openpetra.org
+./addOpenPetraInstance.sh demo localhost
+
+# download the demo database
+cd /home/op_demo
+wget https://github.com/openpetra/demo-databases/raw/UsedForNUnitTests/demoWith1ledger.yml.gz
+chown op_demo:op_demo demoWith1ledger.yml.gz
+cd -
+
+# restore the demo database
+./restoreDemoDatabase.sh
+
+# install cronjob
+crontab -l || echo | crontab -
+if [[ "`crontab -l | grep openpetra/restoreDemoDatabase.sh`" == "" ]]
+then
+  pwd=`pwd`
+  (crontab -l && echo "45 1 * * * $pwd/restoreDemoDatabase.sh" ) | crontab -
+  systemctl enable crond
+  systemctl start crond
+fi
+
---
+++ openpetra/restoreDemoDatabase.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author: Timotheus Pokorra <tp@tbits.net>
+# Copyright: 2017 TBits.net
+# Description: restore the demo database
+
+file=/home/op_demo/demoWith1ledger.yml.gz
+if [ -f $file ]
+then
+  userName=op_demo NAME=op_demo /usr/bin/openpetra-server loadYmlGz $file || exit -1
+  systemctl restart op_demo || exit -1
+fi
+
