3bc082e7c903b5ff9df9a2b171cf93854c9d6b1c
Timotheus Pokorra <tp@tbits.net>
2018-05-15 05:38:05 +0200

    use Mono >= 5.10 from Xamarin

--- openpetra/reinstall.sh
+++ openpetra/reinstall.sh
@@ -53,6 +53,11 @@ cd -
 
 yum clean all
 yum -y install epel-release
+
+# install Xamarin repository for Mono >= 5.10
+rpm --import "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+su -c 'curl https://download.mono-project.com/repo/centos7-stable.repo | tee /etc/yum.repos.d/mono-centos7-stable.repo'
+
 yum -y install openpetranow-mysql-test || exit -1
 
 if [ -f /tmp/openpetra-server ]
