91e22de31a1a40ffcfa68b1d6e070b0cb46741c7
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2018-12-28 23:10:17 +0100

    set settings in all config files

---
+++ openpetra/configSettings.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Author: Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
+# Copyright: 2018 Solidcharity.com
+# Description: set various config settings for the instance(s)
+
+if [ -z "$1" ]
+then
+  echo "call: . $0 all"
+  echo "call: . $0 <customer>"
+  exit -1
+fi
+
+customer=$1
+
+function set_config {
+  customer=$1
+
+  if [ -f /usr/lib/systemd/system/$customer.service ]
+  then
+    if [[ "`systemctl is-enabled $customer`" == "enabled" ]]
+    then
+      cfgfile="/home/$customer/etc/PetraServerConsole.config"
+
+      # drop the line
+      #cat $cfgfile | grep -v "Server.EmailDomain" > $cfgfile.new
+      #mv $cfgfile.new $cfgfile
+
+      # replace existing value
+      sed -i 's/"Server.EmailDomain" value=".*"/"Server.EmailDomain" value="openpetra.com"/g' $cfgfile
+
+      # add new line
+      # grep -q returns 0 if text was found
+      if [ `cat $cfgfile | grep -q 'Server.EmailDomain'` ]
+      then
+        sed -i 's#    <add key="ApplicationDirectory"#    <add key="Server.EmailDomain" value="openpetra.com"/>\n    <add key="ApplicationDirectory"#g' $cfgfile
+      fi
+
+      echo "restarting $customer"
+      systemctl restart $customer
+    fi
+  fi
+}
+
+if [[ "$customer" == "all" ]]
+then
+  for d in /home/openpetra /home/op_*
+  do
+    if [ -d $d ]
+    then
+      service=`basename $d`
+      set_config $service
+    fi
+  done
+else
+  set_config $customer
+fi
