3e18cf292abd88d505674f7219cfc23c0eebd38a
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-08-19 07:15:41 +0200

    more fixes for docker images

--- docker/files/init.sh
+++ docker/files/init.sh
@@ -10,12 +10,12 @@ if [ ! -d /root/openpetra/csharp ]
 then
   # copy the working directory to the mounted directory so that it is outside of the container
   cp -R /root/.openpetra/. /root/openpetra/
+  chmod a+wr -R /root/openpetra
 fi
 
-#alias cp=cp
-#cp -f /root/openpetra/setup/petra0300/linuxserver/mysql/centos/openpetra-server.service /usr/lib/systemd/system/openpetra.service
-#cp -f /root/openpetra/setup/petra0300/linuxserver/mysql/centos/openpetra-server.sh /usr/bin/openpetra-server && chmod a+x /usr/bin/openpetra-server
-#cp -f /root/openpetra/delivery/db/*.sql /usr/local/openpetra/db/
+alias cp=cp
+cp -f /root/openpetra/setup/petra0300/linuxserver/mysql/centos/openpetra-server.service /usr/lib/systemd/system/openpetra.service
+cp -f /root/openpetra/setup/petra0300/linuxserver/mysql/centos/openpetra-server.sh /usr/bin/openpetra-server && chmod a+x /usr/bin/openpetra-server
 
 rm -Rf /usr/local/openpetra/client && ln -s /root/openpetra/js-client /usr/local/openpetra/client
 rm -Rf /usr/local/openpetra/reports && ln -s /root/openpetra/XmlReports /usr/local/openpetra/reports
@@ -27,10 +27,12 @@ then
   cd /root/openpetra
   nant install -D:with-restart=false
 
+  cd /usr/local/openpetra/server && ln -s . api && ln -s ../bin
+  cd /usr/local/openpetra/bin && ln -s /usr/lib64/libgdiplus.so
+  cd /usr/local/openpetra/bin && ln -s /usr/lib64/libsodium.so.23 libsodium.so
+
   # install the database and openpetra instance
   export OPENPETRA_DBPWD="ThisIsJustForDevelopment1234"
-  cp /root/openpetra/delivery/bin/*.exe /usr/local/openpetra/bin
-  cp /root/openpetra/delivery/bin/*.dll /usr/local/openpetra/bin
   /usr/bin/openpetra-server init && /usr/bin/openpetra-server initdb
   cp /root/*.yml.gz /home/openpetra
   chown openpetra:openpetra /home/openpetra/*.yml.gz
