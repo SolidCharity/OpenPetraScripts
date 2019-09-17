3c9e4263022838db5721a59c3ed819cfd11cfd55
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-09-17 06:58:40 +0200

    add instance: new way of running instances on one single Mono process

--- openpetra/addOpenPetraInstance.sh
+++ openpetra/addOpenPetraInstance.sh
@@ -4,10 +4,14 @@
 # Copyright: 2019 SolidCharity.com
 # Description: add a new OpenPetra instance, creating/overwriting the database
 
+export FIRST_HTTP_PORT=7100
+
 if [ -z "$1" ]
 then
-  echo "call: URL=<domain> SYSADMIN_PWD=<inital_passwd> OPENPETRA_PORT=<port> $0 <customer> localhost"
+  echo "call: URL=<domain> PREFIX=<prefix> SYSADMIN_PWD=<inital_passwd> [OPENPETRA_HTTP_PORT=<port>] $0 <customer> localhost"
   echo "call: URL=<domain> $0 <customer> <dbhost> <dbname> <dbuser> <dbpwd> <sysadminpwd>"
+  echo " eg. URL=openpetra.org PREFIX= SYSADMIN_PWD=topsecret ./addOpenPetraInstance.sh test500 localhost"
+  echo " eg. URL=openpetra.org PREFIX=op SYSADMIN_PWD=topsecret ./addOpenPetraInstance.sh 012345 localhost"
   exit -1
 fi
 
@@ -38,9 +42,9 @@ function FindFreePort()
 export customer=$1
 export OP_CUSTOMER=op_$customer
 export userName=op_$customer
-export OPENPETRA_URL=op$customer.$URL
+export OPENPETRA_URL=$PREFIX$customer.$URL
 export OPENPETRA_EMAILDOMAIN=$URL
-export OPENPETRA_HTTP_URL=https://op$customer.$URL
+export OPENPETRA_HTTP_URL=https://$OPENPETRA_URL
 export OPENPETRA_DBHOST=$2
 if [[ "$OPENPETRA_DBHOST" == "localhost" ]]
 then
@@ -54,14 +58,13 @@ else
   export SYSADMIN_PWD=$6
 fi
 
-if [ -z "$OPENPETRA_PORT" ]
+if [ -z "$OPENPETRA_HTTP_PORT" ]
 then
-  FindFreePort 9000
-  export OPENPETRA_PORT=$id
+  FindFreePort $FIRST_HTTP_PORT
+  export OPENPETRA_HTTP_PORT=$id
 fi
-export OPENPETRA_HTTP_PORT=$((OPENPETRA_PORT-1000))
 # add service
-echo -e "op_$customer\t$OPENPETRA_PORT/tcp\t\t\t# OpenPetra for $customer" >> /etc/services
+echo -e "op_$customer\t$OPENPETRA_HTTP_PORT/tcp\t\t\t# OpenPetra for $customer" >> /etc/services
 
 openpetra-server init || exit -1
 openpetra-server initdb || exit -1
@@ -71,9 +74,6 @@ then
   cfgfile="/home/op_$customer/etc/PetraServerConsole.config"
   sed -i "s#    <add key=\"ApplicationDirectory\"#    <add key=\"AuthTokenForInitialisation\" value=\"$AUTHTOKENINIT\"/>\n    <add key=\"ApplicationDirectory\"#g" $cfgfile
 fi
-
-# restart the instance to clean up the RAM
-systemctl restart op_$customer
 
 echo "make sure you have configured the tunnel for the web access: "
 echo "./initWebproxy.sh 150 $OPENPETRA_URL dummy $OPENPETRA_HTTP_PORT"
